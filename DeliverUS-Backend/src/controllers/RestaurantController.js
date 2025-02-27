bba844069b8eacdd7886291b1eafbcd2204a78fd
billalbelkho <bilalbelkho2001@gmail.com>
2025-02-27 11:52:19 +0100

    controller and routes

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -22,19 +22,57 @@ const index = async function (req, res) {
 // TODO: Complete the following functions
 
 const create = async function (req, res) {
-
+  const newRestaurant = Restaurant.build(req.body)
+  // newRestaurant.userId = req.user.id // authenticated user
+  newRestaurant.userId = 1
+  try {
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const show = async function (req, res) {
-
+  const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+    attributes: { exclude: ['userId'] },
+    include: [{
+      model: Product,
+      as: 'products',
+      include: { model: ProductCategory, as: 'productCategory' }
+    },
+    {
+      model: RestaurantCategory,
+      as: 'restaurantCategory'
+    }],
+    order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+  }
+  )
+  try {
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const update = async function (req, res) {
-
+  const restaurantUpdate = await Restaurant.findByPk(req.params.restaurantId)
+  try {
+    const updatedRestaurant = await restaurantUpdate.update(req.body)
+    res.json(updatedRestaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
-
+  const deleterestaurant = await Restaurant.findByPk(req.params.restaurantId)
+  try {
+    const restaurant = deleterestaurant.destroy()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const RestaurantController = {
